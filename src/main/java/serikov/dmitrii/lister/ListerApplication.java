5e6880b1d6281af5f8c01da6cfbf79cf029b7716
DmitriiSer <dmitrii.serikov@gmail.com>
2021-07-20 08:30:50 -0400

    Added Spring Boot application and test classes.

---
+++ src/main/java/serikov/dmitrii/lister/ListerApplication.java
@@ -0,0 +1,13 @@
+package serikov.dmitrii.lister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListerApplication.class, args);
+	}
+
+}
