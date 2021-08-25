122a4329234b64808f31395de73a433ea4a00bff
DmitriiSer <dmitrii.serikov@gmail.com>
2021-08-25 00:11:05 -0400

    Added new routes.

--- src/main/ui/src/app/app-routing.module.ts
+++ src/main/ui/src/app/app-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './modules/home/home.component';
+import { ListsComponent } from './modules/lists/lists.component';
 import { LoginComponent } from './modules/login/login.component';
+import { NewListComponent } from './modules/lists/new-list/new-list.component';
 
 import { AuthGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] }
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'lists', component: ListsComponent, canActivate: [AuthGuard] },
+  { path: 'lists/new', component: NewListComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
