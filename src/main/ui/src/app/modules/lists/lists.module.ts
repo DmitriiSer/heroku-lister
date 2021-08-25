6d15aec54bc4fc433c8c80451ce5292443c23075
DmitriiSer <dmitrii.serikov@gmail.com>
2021-08-25 00:11:40 -0400

    Added ListsComponent.

--- src/main/ui/src/app/modules/lists/lists.module.ts
+++ src/main/ui/src/app/modules/lists/lists.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 
 import { NewListComponent } from './new-list/new-list.component';
+import { ListsComponent } from './lists.component';
 
 @NgModule({
   declarations: [
-    NewListComponent
+    NewListComponent,
+    ListsComponent
   ],
   imports: [
     CommonModule,
