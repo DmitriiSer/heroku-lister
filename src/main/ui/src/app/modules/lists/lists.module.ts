9205f03b9a036a931e59ae84f36ea5087aae8d00
DmitriiSer <dmitrii.serikov@gmail.com>
2021-09-03 16:26:14 -0400

    Allowing to change new list title.

--- src/main/ui/src/app/modules/lists/lists.module.ts
+++ src/main/ui/src/app/modules/lists/lists.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -16,6 +17,7 @@ import { ListsComponent } from './lists.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     //
     MatButtonModule,
     MatFormFieldModule,
--- src/main/ui/src/app/modules/lists/new-list/new-list.component.ts
+++ src/main/ui/src/app/modules/lists/new-list/new-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, NgZone, ViewChild } from '@angular/core';
 import { Location } from '@angular/common';
 
 @Component({
@@ -6,21 +6,43 @@ import { Location } from '@angular/common';
   templateUrl: './new-list.component.html',
   styleUrls: ['./new-list.component.scss']
 })
-export class NewListComponent implements OnInit {
+export class NewListComponent {
+
+  listTitle = '';
+  editTitle = false;
+
+  @ViewChild('titleInput') titleInput!: ElementRef;
 
   constructor(
+    private zone: NgZone,
     private location: Location
   ) { }
 
-  ngOnInit(): void {
-  }
 
   backdropClick() {
     this.location.back();
   }
 
-  saveList() {
+  listTitleFocus() {
+    this.editTitle = true;
+    this.zone.run(() => {
+      setTimeout(() => {
+        this.titleInput.nativeElement.focus();
+      });
+    });
+  }
 
+  listTitleKeyPress(key: string) {
+    if (key === 'Enter') {
+      this.listTitleChange();
+    }
+  }
+
+  listTitleChange() {
+    this.editTitle = false;
+  }
+
+  saveList() {
   }
 
 }
