35d2b107f4011635a17a0487bff0812d5e6bc114
DmitriiSer <dmitrii.serikov@gmail.com>
2021-09-03 14:26:38 -0400

    Navigate to the previous page when user clicks out of the new list box.

--- src/main/ui/src/app/modules/lists/new-list/new-list.component.ts
+++ src/main/ui/src/app/modules/lists/new-list/new-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-new-list',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  backdropClick() {
+    this.location.back();
   }
 
   saveList() {
